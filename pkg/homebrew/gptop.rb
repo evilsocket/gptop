b4235dac3057a244ac4a92255cbedfb8153b569a
evilsocket <evilsocket@gmail.com>
2026-03-03 20:32:50 +0100

    misc: small fix or general refactoring i did not bother commenting

--- pkg/homebrew/gptop.rb
+++ pkg/homebrew/gptop.rb
@@ -3,7 +3,7 @@ class Gptop < Formula
   homepage "https://github.com/evilsocket/gptop"
   version "0.1.1"
   url "https://github.com/evilsocket/gptop/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "UPDATE_ME"
+  sha256 "bdb7411ff57e99ebdd6464d75dc4259c328e3440bd88ab4049640a1061878a51"
   license "GPL-3.0"
 
   depends_on "rust" => :build
